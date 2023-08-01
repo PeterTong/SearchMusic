0c32459fa12ebb87f54dc11f633d875b8a78e621
tkwpeter21 <tkwpeter21@gmail.com>
2023-08-01 18:18:36 +0800

    Initial Commit

---
+++ SearchMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchMusic
+//
+//  Created by KwokWing Tong on 1/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

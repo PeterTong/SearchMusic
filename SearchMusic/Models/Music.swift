b59a6c8ac5d21aa0ebb549ede4be28ddf9856502
tkwpeter21 <tkwpeter21@gmail.com>
2023-08-02 21:58:48 +0800

    Add App Icon and Music Model

---
+++ SearchMusic/Models/Music.swift
@@ -0,0 +1,14 @@
+//
+//  Music.swift
+//  SearchMusic
+//
+//  Created by KwokWing Tong on 2/8/2023.
+//
+
+import Foundation
+
+struct Music: Decodable{
+	let song: String
+	let album: String
+	let artist: String
+}
